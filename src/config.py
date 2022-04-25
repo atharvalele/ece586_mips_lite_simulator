63eae2cb595f0a2ccecae07aa79265bdfeffdaff
Atharva Lele <itsatharva@gmail.com>
2022-04-24 18:04:13 -0700

    config: initial config
    Signed-off-by: Atharva Lele <itsatharva@gmail.com>

---
+++ src/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+config: Global configurations and defines for the project
+"""
+
+# Logging configuration
+LOG_FORMAT = '%(module)s/%(funcName)s (%(lineno)d)\t\t: %(message)s'
+
+# Memory Size in bytes
+MEM_SIZE = 4096
