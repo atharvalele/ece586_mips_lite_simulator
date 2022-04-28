357c219f95de82166aadd06dd562c1544e542614
shivani palkar <shivanipalkar25@gmail.com>
2022-04-28 13:23:40 -0700

    parser: initial

---
+++ src/parser.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+parser.py: Parsing memory image
+
+Author(s): Shivani Palkar <spalkar@pdx.edu>
+"""
+
+def parser(mem_image: str) -> bytearray:
+    image = bytearray()
+    mem_file = open(mem_image, "r")
+
+    for string in mem_file:
+        to_byte = bytearray.fromhex(string)
+        image += to_byte
+    
+    return image
