c488adf4597de652abcb4888f1bcba52c7ff0a61
shivani palkar <shivanipalkar25@gmail.com>
2022-05-01 12:29:19 -0700

    cpu: decode function added

--- src/cpu.py
+++ src/cpu.py
@@ -10,6 +10,55 @@ import config
 import memory
 import logging
 
+class Instruction:
+    # R-type
+    ADD = 0b000000
+    SUB = 0b000010
+    MUL = 0b000100
+    OR = 0b000110
+    AND = 0b001000
+    XOR = 0b001010
+
+    # I- type
+    ADDI = 0b000001
+    SUBI = 0b000011
+    MULI = 0b000101
+    ORI = 0b000111
+    ANDI = 0b001001
+    XORI = 0b001011
+    LDW  = 0b001100
+    STW = 0b001101
+    BZ = 0b001110
+    BEQ = 0b001111
+    JR = 0b010000
+    HALT = 0b010001
+
+    R_type_instr = [ADD, SUB, MUL, OR, AND, XOR]
+    I_type_instr = [ADDI, SUBI, MULI, ORI, ANDI, XORI, LDW, STW, BZ, BEQ, JR, HALT]
+
+    def __init__(self, instr: int) -> None:
+        self.instr = instr
+
+    def decode(self) -> None:
+        self.opcode = (self.instr & 0xFC000000) >> 26
+
+        if self.opcode in self.R_type_instr:
+            self.type = 'R' 
+            self.rs = (self.instr & 0x03E00000) >> 19
+            self.rt = (self.instr & 0x001F0000) >> 16
+            self.rd = (self.instr & 0x0000F800) >> 11
+            self.imm = None
+        elif self.opcode in self.I_type_instr:
+            self.type = 'I' 
+            self.rs = (self.instr & 0x03E00000) >> 19
+            self.rt = (self.instr & 0x001F0000) >> 16
+            self.rd = None
+            self.imm = self.instr & 0x000007FF
+        else:
+            logging.error ('Invalid opcode: ' + bin(self.opcode)) 
+
+    
+
 class MIPS_lite:
     # Init
     def __init__(self, mode: str, mem_fname: str, out_fname: str) -> None:
