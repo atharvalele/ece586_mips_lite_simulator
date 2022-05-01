60cf7d4d7e5baa708daf5e03305be9e2b6562105
mehulshah225 <42416898+mehulshah225@users.noreply.github.com>
2022-04-30 18:36:13 -0700

    test: assembler:  r type instr implementation

--- test/assembler.py
+++ test/assembler.py
@@ -3,7 +3,7 @@
 """
 assembler.py: File for testing the project
 
-Author(s): Ayush Srivastava <ayush@pdx.edu>
+Author(s): Ayush Srivastava <ayush@pdx.edu> Mehul Shah <mehul@pdx.edu>
 """
 
 import sys
@@ -32,15 +32,32 @@ def op_code_check(opcode: str):
     }
     return switcher.get(opcode , "Invalid OP Code")
 
+def r_check(rd: str):
+    print
+    reg_no = bin(int(rd[-1])).replace("0b","")
+
+    r = str(reg_no)
+    r = r.zfill(5)
+    return r
+
+
 def convert_to_mem(output: str, command):
     # Extract tokens from list
     for i in command:
         op_list = i.split()
         op = op_code_check(op_list[0])
-        print(op)
+        if (int(op) in [0,2,4,6,8,10]):
+            rd = r_check(op_list[1][:-1])
+            rs = r_check(op_list[2][:-1])
+            rt = r_check(op_list[3])
+            print(f"{op} {rs} {rt} {rd}")
+            trace = op + rs + rt + rd
+            trace = trace.ljust(32, "0")
+            trace = hex(int(trace, 2)).replace("0x","")
+            trace = trace.zfill(8)
+            print(trace)
+            
         
-
-
 if __name__ == '__main__':
     # Start by checking length of parameters
     if (len(sys.argv) < 3):
@@ -54,7 +71,7 @@ if __name__ == '__main__':
     command_list = f.readlines()
 
     # Print to test all commands were picked up
-    # print(command_list)
+    print(command_list)
 
     # call function to putput to text file 
     convert_to_mem(sys.argv[2], command_list)
