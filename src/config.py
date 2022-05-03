4738fe7123a8f287fe6103b055edeece8b9ffbf8
Atharva Lele <itsatharva@gmail.com>
2022-05-03 12:05:05 -0700

    config: remove tabs from debug print format
    Signed-off-by: Atharva Lele <itsatharva@gmail.com>

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ config: Global configurations and defines for the project
 """
 
 # Logging configuration
-LOG_FORMAT = '%(module)s/%(funcName)s (%(lineno)d)\t\t: %(message)s'
+LOG_FORMAT = '%(module)s/%(funcName)s (%(lineno)d): %(message)s'
 
 # Memory Size in bytes
 MEM_SIZE = 4096
